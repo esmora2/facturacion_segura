e57a4445afe1382ec3c93c108994f004fbefd28a
esmora2 <esmora2@espe.edu.ec>
2025-08-07 10:00:51 -0500

    a

--- facturacion_segura/settings.py
+++ facturacion_segura/settings.py
@@ -124,3 +124,7 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+SILK_AUTHENTICATION = True  # Requiere login
+SILK_AUTHORISATION = True   # Solo staff/superuser puede acceder
+
+
